104ffc303becdcb28912500e6e5d06ff08cb9f16
rieryn <45051404+rieryn@users.noreply.github.com>
2021-03-30 02:59:54 -0400

    responsive styling

--- p2p-test/components/courseTableRow.js
+++ p2p-test/components/courseTableRow.js
@@ -2,8 +2,8 @@
 export default function courseTable (props) {
   
   return (
-    <tr className = "text-left">
-      <td className="px-6 py-4 whitespace-nowrap">
+    <tr className = "text-left max-w-xs">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
         <div className="flex items-center ">
           <div>
             <a href="#" className="text-sm font-medium text-blue-400 hover:text-yellow-300">CSCI 1040U</a>
@@ -11,12 +11,12 @@ export default function courseTable (props) {
           </div>
         </div>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
         <div className="text-sm text-gray-900">100</div>
-        <div className="text-sm text-gray-500">Krogg took 10 damage...</div>
+        <div className="hidden md:block overflow-ellipsis overflow-hidden text-sm text-gray-500">Krogg took 10 damage testing if this wraps properly Krogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properly...</div>
         <a href="#" className="text-sm text-blue-400">(expand)</a>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> **** </span>
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">74%</td>
