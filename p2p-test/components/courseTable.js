34899954789edbefe8498dca52953dc468d58a8f
rieryn <45051404+rieryn@users.noreply.github.com>
2021-03-30 09:09:00 -0400

    weird animation

--- p2p-test/components/courseTable.js
+++ p2p-test/components/courseTable.js
@@ -3,7 +3,7 @@ import CourseTableRow from '../components/courseTableRow'
 export default function courseTable () {
   
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
--- p2p-test/components/courseTableRow.js
+++ p2p-test/components/courseTableRow.js
@@ -1,9 +1,9 @@
 
 export default function courseTable (props) {
-  
+  const viewBox = `0 0 50 50`;
   return (
-    <tr className = "text-left max-w-xs">
-      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
+    <tr className = "text-left ">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs transition duration-500 ease-in-out hover: transform hover:-translate-y-1 hover:scale-101">
         <div className="flex items-center ">
           <div>
             <a href="#" className="text-sm font-medium text-blue-400 hover:text-yellow-300">CSCI 1040U</a>
@@ -11,15 +11,17 @@ export default function courseTable (props) {
           </div>
         </div>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs transition duration-500 ease-in-out hover: transform hover:-translate-y-1 hover:scale-101">
         <div className="text-sm text-gray-900">100</div>
         <div className="hidden md:block overflow-ellipsis overflow-hidden text-sm text-gray-500">Krogg took 10 damage testing if this wraps properly Krogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properlyKrogg took 10 damage testing if this wraps properly...</div>
         <a href="#" className="text-sm text-blue-400">(expand)</a>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap max-w-xs">
+      <td className="px-6 py-4 whitespace-nowrap max-w-xs transition duration-500 ease-in-out hover: transform hover:-translate-y-1 hover:scale-101">
         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> **** </span>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">74%</td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 transition duration-500 ease-in-out hover: transform hover:-translate-y-1 hover:scale-101">
+        74%
+      </td>
     </tr>
               
   )
