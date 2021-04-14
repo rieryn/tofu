0e5395f1d8e0a634aac45725b700e25ee37ab5ef
rieryn <45051404+rieryn@users.noreply.github.com>
2021-04-14 03:57:51 -0400

    update components to use new data

--- p2p-test/pages/course_search_results.js
+++ p2p-test/pages/course_search_results.js
@@ -7,7 +7,7 @@ export default function CourseSearch () {
   console.log(router.query);
   return (
     <Frame>
-      <div className = "pt-48 md:p-36 lg:p-48 lg:pr-64 lg:pl-64">
+      <div className = "pt-48 md:p-24 lg:p-36 xl:p-48 ">
       Course search results
       <CourseTable />
       </div>
